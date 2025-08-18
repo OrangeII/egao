99cb7130002bcf8ef5fffdc42ac56d0e33e0af5d
OrangeII <ivaldinico@gmail.com>
2025-08-18 18:14:40 +0200

    dev

--- src/parts.ts
+++ src/parts.ts
@@ -14,15 +14,36 @@ const parts: Part = {
   },
   face: {
     happy: "￣ ▽ ￣",
+    angry: "ಠ 益 ಠ",
   },
+  eyes: {
+    line: {
+      bliss: "￣",
+      happy: "⁀",
+    },
+    o: {
+      disapproving: "ಠ",
+      tiny: "°",
+      happy: "◕",
+      sad: "⊙",
+      "side-look": "￢",
+      excited: {
+        left: ">",
+        right: "<",
+      },
+      "very-excited": {
+        left: "≧",
+        right: "≦",
+      },
+    },
+  },
+  //accessories
   blush: {
     content: "⁄ ⁄ ⁄",
     "white-space": "pre",
   },
-  star: {
-    content: "✩",
-    "white-space": "pre",
-  },
+  veins: "╬",
+  sweat: "；",
 };
 
 export { parts, Part };
