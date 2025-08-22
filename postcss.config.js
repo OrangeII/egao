806eb8cb93cd1dea981850eb03cc5146178ad617
OrangeII <ivaldinico@gmail.com>
2025-08-22 11:08:57 +0200

    fix import cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 import autoprefixer from "autoprefixer";
 import buildPartClasses from "./build/buildPartClasses.js";
-// import cssnano from "cssnano";
+import cssnano from "cssnano";
 
 export default {
   plugins: [buildPartClasses, autoprefixer, cssnano({ preset: "default" })],
